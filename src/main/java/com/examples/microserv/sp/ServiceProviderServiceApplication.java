4049abb211725dcc7d542109eee3bf646142ecfb
sshomvi <sshomvi@gmail.com>
2019-07-17 17:22:51 +0530

    Initial commit

---
+++ src/main/java/com/examples/microserv/sp/ServiceProviderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.examples.microserv.sp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceProviderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceProviderServiceApplication.class, args);
+	}
+
+}
